3d668a2141b1d42ab22aac32c6f71db569c6ba72
userG <1023536640@qq.com>
2023-09-25 20:41:11 +0800

    前端

---
+++ src/settings.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: userG 1023536640@qq.com
+ * @Date: 2023-09-25 19:46:41
+ * @LastEditors: userG 1023536640@qq.com
+ * @LastEditTime: 2023-09-25 20:24:21
+ * @FilePath: \x-admin-web\src\settings.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+
+  title: 'CapybaraToFirst',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
